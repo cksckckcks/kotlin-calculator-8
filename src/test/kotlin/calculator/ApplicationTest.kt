77032f6935e6a539d384b6c54e1b667ebdb28e72
cksckckcks <chan679999@naver.com>
2025-10-19 17:46:04 +0900

    test(calculator): add test code for calculator

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -22,6 +22,50 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @Test
+    fun `구분자가 1글자가 아닌 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("//aa\\n1aa2aa3") }
+        }
+    }
+
+    @Test
+    fun `구분자 형식이 잘못된 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("//aa//1aa2aa3") }
+        }
+    }
+
+    @Test
+    fun `숫자에 음수가 들어온 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("//a\\n1a-2a3") }
+        }
+    }
+
+    @Test
+    fun `숫자가 아닌 문자가 있을 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1:2:a:3") }
+        }
+    }
+
+    @Test
+    fun `구분자가 연속으로 사용된 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1:2::3") }
+        }
+    }
+
+    @Test
+    fun `기본 구분자 2가지 모두 사용한 경우`() {
+        assertSimpleTest {
+            run("1,1,2:3,3")
+            assertThat(output()).contains("결과 : 10")
+        }
+    }
+
+
     override fun runMain() {
         main()
     }
