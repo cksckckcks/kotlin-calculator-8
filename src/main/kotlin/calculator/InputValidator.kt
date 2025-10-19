8d7b444321374b07996fcbf355075b1d065c525c
cksckckcks <chan679999@naver.com>
2025-10-19 15:08:36 +0900

    feat(exception): exception handling

--- src/main/kotlin/calculator/InputValidator.kt
+++ src/main/kotlin/calculator/InputValidator.kt
@@ -4,16 +4,16 @@ class InputValidator {
     fun validateAndConvert(parts: List<String>): List<Int> {
         return parts.map { part ->
             if (part.isEmpty()) {
-                // Exception
+                throw IllegalArgumentException("구분자 사용이 잘못되었습니다.")
             }
 
-            val num = part.toIntOrNull() ?: 0 // Exception
+            val number = part.toIntOrNull() ?: throw IllegalArgumentException("입력 값이 숫자가 아닌 값이 있습니다.")
 
-            if (num <= 0) {
-                // Exception
+            if (number <= 0) {
+                throw IllegalArgumentException("입력된 값에 양수가 아닌 수가 있습니다.")
             }
-            
-            num
+
+            number
         }
     }
 }
